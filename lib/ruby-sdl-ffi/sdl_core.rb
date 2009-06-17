36cefa3e3eda108e33f1f6ba6af92641f9a2177a
John Croisant <jacius@gmail.com>
2009-06-17 02:10:10 -0500

    Cosmetic cleanup in sdl_core.rb.

--- lib/ruby-sdl-ffi/sdl_core.rb
+++ lib/ruby-sdl-ffi/sdl_core.rb
@@ -37,22 +37,20 @@ module SDL
     GLATTR      = :int            # :nodoc:
 
 
-    LIL_ENDIAN = 1234
-    BIG_ENDIAN = 4321
+    LIL_ENDIAN  = 1234
+    BIG_ENDIAN  = 4321
 
 
 
     # SDL.h
 
     class Version < FFI::Struct
-      layout(
-             :major, :uint8,
-             :minor, :uint8,
-             :patch, :uint8
-      )
+      layout( :major, :uint8,
+              :minor, :uint8,
+              :patch, :uint8 )
     end
 
-    attach_function :SDL_Linked_Version, [  ], :pointer
+    attach_function  :SDL_Linked_Version, [  ], :pointer
 
     INIT_TIMER       = 0x00000001
     INIT_AUDIO       = 0x00000010
@@ -63,11 +61,11 @@ module SDL
     INIT_EVENTTHREAD = 0x01000000
     INIT_EVERYTHING  = 0x0000FFFF
 
-    attach_function :SDL_Init, [ :uint32 ], :int
-    attach_function :SDL_InitSubSystem, [ :uint32 ], :int
-    attach_function :SDL_QuitSubSystem, [ :uint32 ], :void
-    attach_function :SDL_WasInit, [ :uint32 ], :uint32
-    attach_function :SDL_Quit, [  ], :void
+    attach_function  :SDL_Init,           [ :uint32 ], :int
+    attach_function  :SDL_InitSubSystem,  [ :uint32 ], :int
+    attach_function  :SDL_QuitSubSystem,  [ :uint32 ], :void
+    attach_function  :SDL_WasInit,        [ :uint32 ], :uint32
+    attach_function  :SDL_Quit,           [         ], :void
 
 
 
@@ -77,28 +75,28 @@ module SDL
     APPINPUTFOCUS = 0x02
     APPACTIVE     = 0x04
 
-    attach_function :SDL_GetAppState, [  ], :uint8
+    attach_function  :SDL_GetAppState,  [  ], :uint8
 
 
 
     # SDL_cpuinfo.h
 
-    attach_function :SDL_HasRDTSC,    [  ], SDL::Raw::BOOL
-    attach_function :SDL_HasMMX,      [  ], SDL::Raw::BOOL
-    attach_function :SDL_HasMMXExt,   [  ], SDL::Raw::BOOL
-    attach_function :SDL_Has3DNow,    [  ], SDL::Raw::BOOL
-    attach_function :SDL_Has3DNowExt, [  ], SDL::Raw::BOOL
-    attach_function :SDL_HasSSE,      [  ], SDL::Raw::BOOL
-    attach_function :SDL_HasSSE2,     [  ], SDL::Raw::BOOL
-    attach_function :SDL_HasAltiVec,  [  ], SDL::Raw::BOOL
+    attach_function  :SDL_HasRDTSC,     [  ], SDL::Raw::BOOL
+    attach_function  :SDL_HasMMX,       [  ], SDL::Raw::BOOL
+    attach_function  :SDL_HasMMXExt,    [  ], SDL::Raw::BOOL
+    attach_function  :SDL_Has3DNow,     [  ], SDL::Raw::BOOL
+    attach_function  :SDL_Has3DNowExt,  [  ], SDL::Raw::BOOL
+    attach_function  :SDL_HasSSE,       [  ], SDL::Raw::BOOL
+    attach_function  :SDL_HasSSE2,      [  ], SDL::Raw::BOOL
+    attach_function  :SDL_HasAltiVec,   [  ], SDL::Raw::BOOL
 
 
 
     # SDL_error.h
 
-    attach_function :SDL_SetError, [ :string, :varargs ], :void
-    attach_function :SDL_GetError, [  ], :string
-    attach_function :SDL_ClearError, [  ], :void
+    attach_function  :SDL_SetError,     [ :string, :varargs  ], :void
+    attach_function  :SDL_GetError,     [                    ], :string
+    attach_function  :SDL_ClearError,   [                    ], :void
 
     ENOMEM      = 0
     EFREAD      = 1
@@ -107,26 +105,28 @@ module SDL
     UNSUPPORTED = 4
     LASTERROR   = 5
 
-    attach_function :SDL_Error, [ SDL::Raw::ENUM ], :void
+    attach_function  :SDL_Error,        [ SDL::Raw::ENUM     ], :void
 
 
 
     # SDL_loadso.h
 
-    attach_function :SDL_LoadObject, [ :string ], :pointer
-    attach_function :SDL_LoadFunction, [ :pointer, :string ], :pointer
-    attach_function :SDL_UnloadObject, [ :pointer ], :void
+    attach_function  :SDL_LoadObject,   [ :string            ], :pointer
+    attach_function  :SDL_LoadFunction, [ :pointer, :string  ], :pointer
+    attach_function  :SDL_UnloadObject, [ :pointer           ], :void
 
 
 
     # SDL_thread.h
 
-    attach_function :SDL_CreateThread, [ callback( :createthread_cb, [:pointer], :int ), :pointer ], :pointer
-    attach_function :SDL_ThreadID, [  ], :uint32
-    attach_function :SDL_GetThreadID, [ :pointer ], :uint32
-    attach_function :SDL_WaitThread, [ :pointer, :pointer ], :void
-    attach_function :SDL_KillThread, [ :pointer ], :void
+    attach_function  :SDL_CreateThread,
+                     [ callback( :createthread_cb, [:pointer], :int ),
+                       :pointer ], :pointer
 
+    attach_function  :SDL_ThreadID,     [                    ], :uint32
+    attach_function  :SDL_GetThreadID,  [ :pointer           ], :uint32
+    attach_function  :SDL_WaitThread,   [ :pointer, :pointer ], :void
+    attach_function  :SDL_KillThread,   [ :pointer           ], :void
 
 
   end

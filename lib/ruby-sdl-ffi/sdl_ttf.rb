51d04cf748dfc31d03c8ebfb3db60c1c965b6b51
John Croisant <jacius@gmail.com>
2009-06-20 03:40:13 -0500

    Cosmetic cleanup in sdl_ttf.rb.

--- lib/ruby-sdl-ffi/sdl_ttf.rb
+++ lib/ruby-sdl-ffi/sdl_ttf.rb
@@ -35,6 +35,7 @@ require File.join( File.dirname(__FILE__), "platforms" )
 # For SDL::Color
 require File.join( File.dirname(__FILE__), "sdl_video" )
 
+
 module SDL
   module TTF
     extend FFI::Library
@@ -43,50 +44,99 @@ module SDL
 
     attach_function :TTF_Linked_Version, [  ], :pointer
 
+
     UNICODE_BOM_NATIVE  = 0xFEFF
     UNICODE_BOM_SWAPPED = 0xFFFE
 
+
     attach_function :TTF_ByteSwappedUNICODE, [ :int ], :void
+
+
     attach_function :TTF_Init, [  ], :int
-    attach_function :TTF_OpenFont, [ :string, :int ], :pointer
+
+
+    attach_function :TTF_OpenFont,      [ :string, :int        ], :pointer
     attach_function :TTF_OpenFontIndex, [ :string, :int, :long ], :pointer
-    attach_function :TTF_OpenFontRW, [ :pointer, :int, :int ], :pointer
-    attach_function :TTF_OpenFontIndexRW, [ :pointer, :int, :int, :long ], :pointer
+    attach_function :TTF_OpenFontRW,    [ :pointer, :int, :int ], :pointer
+
+    attach_function :TTF_OpenFontIndexRW,
+                    [ :pointer, :int, :int, :long ], :pointer
+
 
     STYLE_NORMAL    = 0x00
     STYLE_BOLD      = 0x01
     STYLE_ITALIC    = 0x02
     STYLE_UNDERLINE = 0x04
 
-    attach_function :TTF_GetFontStyle, [ :pointer ], :int
-    attach_function :TTF_SetFontStyle, [ :pointer, :int ], :void
-    attach_function :TTF_FontHeight, [ :pointer ], :int
-    attach_function :TTF_FontAscent, [ :pointer ], :int
-    attach_function :TTF_FontDescent, [ :pointer ], :int
-    attach_function :TTF_FontLineSkip, [ :pointer ], :int
-    attach_function :TTF_FontFaces, [ :pointer ], :long
-    attach_function :TTF_FontFaceIsFixedWidth, [ :pointer ], :int
-    attach_function :TTF_FontFaceFamilyName, [ :pointer ], :string
-    attach_function :TTF_FontFaceStyleName, [ :pointer ], :string
-    attach_function :TTF_GlyphMetrics, [ :pointer, :uint16, :pointer, :pointer, :pointer, :pointer, :pointer ], :int
-    attach_function :TTF_SizeText, [ :pointer, :string, :pointer, :pointer ], :int
-    attach_function :TTF_SizeUTF8, [ :pointer, :string, :pointer, :pointer ], :int
-    attach_function :TTF_SizeUNICODE, [ :pointer, :pointer, :pointer, :pointer ], :int
-    attach_function :TTF_RenderText_Solid, [ :pointer, :string, SDL::Color ], :pointer
-    attach_function :TTF_RenderUTF8_Solid, [ :pointer, :string, SDL::Color ], :pointer
-    attach_function :TTF_RenderUNICODE_Solid, [ :pointer, :pointer, SDL::Color ], :pointer
-    attach_function :TTF_RenderGlyph_Solid, [ :pointer, :uint16, SDL::Color ], :pointer
-    attach_function :TTF_RenderText_Shaded, [ :pointer, :string, SDL::Color, SDL::Color ], :pointer
-    attach_function :TTF_RenderUTF8_Shaded, [ :pointer, :string, SDL::Color, SDL::Color ], :pointer
-    attach_function :TTF_RenderUNICODE_Shaded, [ :pointer, :pointer, SDL::Color, SDL::Color ], :pointer
-    attach_function :TTF_RenderGlyph_Shaded, [ :pointer, :uint16, SDL::Color, SDL::Color ], :pointer
-    attach_function :TTF_RenderText_Blended, [ :pointer, :string, SDL::Color ], :pointer
-    attach_function :TTF_RenderUTF8_Blended, [ :pointer, :string, SDL::Color ], :pointer
-    attach_function :TTF_RenderUNICODE_Blended, [ :pointer, :pointer, SDL::Color ], :pointer
-    attach_function :TTF_RenderGlyph_Blended, [ :pointer, :uint16, SDL::Color ], :pointer
+
+    attach_function :TTF_GetFontStyle,         [ :pointer       ], :int
+    attach_function :TTF_SetFontStyle,         [ :pointer, :int ], :void
+    attach_function :TTF_FontHeight,           [ :pointer       ], :int
+    attach_function :TTF_FontAscent,           [ :pointer       ], :int
+    attach_function :TTF_FontDescent,          [ :pointer       ], :int
+    attach_function :TTF_FontLineSkip,         [ :pointer       ], :int
+    attach_function :TTF_FontFaces,            [ :pointer       ], :long
+    attach_function :TTF_FontFaceIsFixedWidth, [ :pointer       ], :int
+    attach_function :TTF_FontFaceFamilyName,   [ :pointer       ], :string
+    attach_function :TTF_FontFaceStyleName,    [ :pointer       ], :string
+
+
+    attach_function :TTF_GlyphMetrics, 
+                    [ :pointer, :uint16, :pointer, 
+                      :pointer, :pointer, :pointer, :pointer ], :int
+
+    attach_function :TTF_SizeText,
+                    [ :pointer, :string, :pointer, :pointer ], :int
+
+    attach_function :TTF_SizeUTF8,
+                    [ :pointer, :string, :pointer, :pointer ], :int
+
+    attach_function :TTF_SizeUNICODE,
+                    [ :pointer, :pointer, :pointer, :pointer ], :int
+
+
+    attach_function :TTF_RenderText_Solid,
+                    [ :pointer, :string, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderUTF8_Solid,
+                    [ :pointer, :string, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderUNICODE_Solid,
+                    [ :pointer, :pointer, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderGlyph_Solid,
+                    [ :pointer, :uint16, SDL::Color ], :pointer
+
+
+    attach_function :TTF_RenderText_Shaded,
+                    [ :pointer, :string, SDL::Color, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderUTF8_Shaded,
+                    [ :pointer, :string, SDL::Color, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderUNICODE_Shaded,
+                    [ :pointer, :pointer, SDL::Color, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderGlyph_Shaded,
+                    [ :pointer, :uint16, SDL::Color, SDL::Color ], :pointer
+
+
+    attach_function :TTF_RenderText_Blended,
+                    [ :pointer, :string, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderUTF8_Blended,
+                    [ :pointer, :string, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderUNICODE_Blended,
+                    [ :pointer, :pointer, SDL::Color ], :pointer
+
+    attach_function :TTF_RenderGlyph_Blended,
+                    [ :pointer, :uint16, SDL::Color ], :pointer
+
+
     attach_function :TTF_CloseFont, [ :pointer ], :void
-    attach_function :TTF_Quit, [  ], :void
-    attach_function :TTF_WasInit, [  ], :int
+    attach_function :TTF_Quit,      [          ], :void
+    attach_function :TTF_WasInit,   [          ], :int
 
   end
 end
